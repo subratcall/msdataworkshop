739dd4a3c98246283758cbf0950721a100a06164
Paul Parkinson <paul.parkinson@gmail.com>
2020-03-03 10:45:24 -0500

    saga services

--- frontend-helidon/src/main/java/io/helidon/data/examples/FrontEndResource.java
+++ frontend-helidon/src/main/java/io/helidon/data/examples/FrontEndResource.java
@@ -51,7 +51,7 @@ public class FrontEndResource {
                              @QueryParam("itemid") String itemid) {
         System.out.println("-----> FrontEnd placeorder orderid:" + orderid + " itemid:" + itemid);
         try {
-            URL url = new URL("http://order.datdademo:8080/placeOrder?orderid=" + orderid +
+            URL url = new URL("http://order.datademo:8080/placeOrder?orderid=" + orderid +
                     "&itemid=" + itemid);
             String json = makeRequest(url);
             System.out.println("FrontEndResource.placeorder json:" + json);
@@ -68,7 +68,7 @@ public class FrontEndResource {
     public String showorder(@QueryParam("orderid") String orderid) {
         System.out.println("-----> FrontEnd showorder orderid:" + orderid);
         try {
-            URL url = new URL("http://order.datdademo:8080/showorder");
+            URL url = new URL("http://order.datademo:8080/showorder");
             return getFullPage(makeRequest(url));
         } catch (IOException e) {
             e.printStackTrace();
@@ -113,7 +113,7 @@ public class FrontEndResource {
         try {
             System.out.println("-----> FrontEnd orderadmin sql = [" + sql + "], user = [" + user + "], password = [" + password + "]");
             sql = URLEncoder.encode(sql, "UTF-8");
-            String urlString = "http://orderadmin.datdademo:8080/execute?sql=" + sql + "&user=" + user + "&password=" + password;
+            String urlString = "http://orderadmin.datademo:8080/execute?sql=" + sql + "&user=" + user + "&password=" + password;
             System.out.println("FrontEndResource.orderadmin urlString:" + urlString);
             URL url = new URL( urlString);
             return getFullPage(makeRequest(url));
@@ -130,7 +130,7 @@ public class FrontEndResource {
         try {
             System.out.println("-----> FrontEnd helidonatpinventory: [" + sql + "], user = [" + user + "], password = [" + password + "]");
             sql = URLEncoder.encode(sql, "UTF-8");
-            String urlString = "http://inventoryadmin.datdademo:8080/execute?sql=" + sql + "&user=" + user + "&password=" + password;
+            String urlString = "http://inventoryadmin.datademo:8080/execute?sql=" + sql + "&user=" + user + "&password=" + password;
             System.out.println("FrontEndResource.inventoryadmin urlString:" + urlString);
             URL url = new URL( urlString);
             return getFullPage(makeRequest(url));
@@ -163,7 +163,7 @@ public class FrontEndResource {
     @Path("/supplierservicecall")
     public String supplierservicecall(@QueryParam("test") String test, @QueryParam("itemid") String itemid) {
         try {
-            URL url = new URL("http://supplier.datdademo:8080/supplier/" + test + "?itemid=" + itemid);
+            URL url = new URL("http://supplier.datademo:8080/supplier/" + test + "?itemid=" + itemid);
             return getFullPage(makeRequest(url));
         } catch (IOException e) {
             e.printStackTrace();
@@ -254,18 +254,22 @@ public class FrontEndResource {
 //                "<textarea cols=\"100\" id = \"geojson\" name=\"geojson\" rows=\"19\">" +
 //                "{&quot;type&quot;:&quot;LineString&quot;,&quot;coordinates&quot;:[[-74.00501,40.70583],[-74.00457,40.70549],[-74.00447,40.70541],[-74.00418,40.70559],[-74.00386,40.70579],[-74.00361,40.70595],[-74.00346,40.70605],[-74.00335,40.70611],[-74.00318,40.70621],[-74.00231,40.7067],[-74.00274,40.70722],[-74.00311,40.70767],[-74.00336,40.708],[-74.00345,40.70808],[-74.00407,40.70745],[-74.00412,40.70757],[-74.00433,40.70783],[-74.00477,40.70841],[-74.00505,40.70876],[-74.00513,40.70885],[-74.00524,40.70893],[-74.00532,40.70899],[-74.00547,40.70909],[-74.00643,40.70956],[-74.00705,40.70987],[-74.00774,40.71022],[-74.00906,40.71089],[-74.01046,40.71153],[-74.01013,40.71209],[-74.00967,40.71274],[-74.00927,40.71326],[-74.00902,40.71359],[-74.00885,40.71381],[-74.0084,40.71437],[-74.00795,40.71494],[-74.00755,40.71544],[-74.00882,40.71602],[-74.0092,40.71619],[-74.00911,40.71692],[-74.00906,40.71726],[-74.009,40.7176],[-74.00894,40.71793],[-74.00888,40.71827],[-74.00882,40.71864],[-74.00875,40.71903],[-74.0087,40.7193],[-74.00858,40.71996],[-74.00847,40.72065],[-74.00842,40.72089],[-74.00837,40.7212],[-74.00834,40.72133],[-74.00823,40.72198],[-74.00812,40.72264],[-74.00801,40.72328],[-74.00795,40.72365],[-74.00793,40.72376],[-74.00786,40.72382],[-74.00777,40.72388],[-74.00773,40.72392],[-74.00771,40.72393],[-74.00745,40.72412],[-74.00736,40.72417],[-74.00728,40.72424],[-74.00723,40.72429],[-74.0071,40.72441],[-74.00703,40.7245]]}" +
 //                "</textarea>" +
-//                "<input type=\"hidden\" id = \"geojson\" name=\"geojson\" value=\"" +
-//                "{&quot;type&quot;:&quot;LineString&quot;,&quot;coordinates&quot;:[[-74.00501,40.70583],[-74.00457,40.70549],[-74.00447,40.70541],[-74.00418,40.70559],[-74.00386,40.70579],[-74.00361,40.70595],[-74.00346,40.70605],[-74.00335,40.70611],[-74.00318,40.70621],[-74.00231,40.7067],[-74.00274,40.70722],[-74.00311,40.70767],[-74.00336,40.708],[-74.00345,40.70808],[-74.00407,40.70745],[-74.00412,40.70757],[-74.00433,40.70783],[-74.00477,40.70841],[-74.00505,40.70876],[-74.00513,40.70885],[-74.00524,40.70893],[-74.00532,40.70899],[-74.00547,40.70909],[-74.00643,40.70956],[-74.00705,40.70987],[-74.00774,40.71022],[-74.00906,40.71089],[-74.01046,40.71153],[-74.01013,40.71209],[-74.00967,40.71274],[-74.00927,40.71326],[-74.00902,40.71359],[-74.00885,40.71381],[-74.0084,40.71437],[-74.00795,40.71494],[-74.00755,40.71544],[-74.00882,40.71602],[-74.0092,40.71619],[-74.00911,40.71692],[-74.00906,40.71726],[-74.009,40.7176],[-74.00894,40.71793],[-74.00888,40.71827],[-74.00882,40.71864],[-74.00875,40.71903],[-74.0087,40.7193],[-74.00858,40.71996],[-74.00847,40.72065],[-74.00842,40.72089],[-74.00837,40.7212],[-74.00834,40.72133],[-74.00823,40.72198],[-74.00812,40.72264],[-74.00801,40.72328],[-74.00795,40.72365],[-74.00793,40.72376],[-74.00786,40.72382],[-74.00777,40.72388],[-74.00773,40.72392],[-74.00771,40.72393],[-74.00745,40.72412],[-74.00736,40.72417],[-74.00728,40.72424],[-74.00723,40.72429],[-74.0071,40.72441],[-74.00703,40.7245]]}" +
-//                "\"/>"  +
-//                "<input type=\"submit\" value=\"deliveryDetail\"></form>" +
                 " <label>From:</label>\n" +
                 "        <input type=\"text\" size=\"35\" id=\"start_address\" name=\"start_address\" value=\"1469 WEBSTER ST,San Francisco,CA\">\n" +
                 "        <label>To:</label>\n" +
                 "        <input type=\"text\" size=\"35\" id=\"end_address\" name=\"end_address\" value=\"780 PANORAMA DR,San Francisco,CA\">\n" +
-//                "        <BR><BR>\n" +
+
+
+
 //                "        <button type=\"button\" onclick=\"find_path_address()\">Submit</button>\n" +
-                "        <button type=\"button\" onclick=\"find_path_address_render()\">deliveryDetail</button>\n" +
-                "        <div id=\"path_address_res\"></div>" +
+//                "        <button type=\"button\" onclick=\"find_path_address_render()\">deliveryDetail</button>" +
+//                "        <div id=\"path_address_res\"></div>" +
+
+                "<input type=\"hidden\" id = \"geojson\" name=\"geojson\" value=\"" +
+                "{&quot;type&quot;:&quot;LineString&quot;,&quot;coordinates&quot;:[[-74.00501,40.70583],[-74.00457,40.70549],[-74.00447,40.70541],[-74.00418,40.70559],[-74.00386,40.70579],[-74.00361,40.70595],[-74.00346,40.70605],[-74.00335,40.70611],[-74.00318,40.70621],[-74.00231,40.7067],[-74.00274,40.70722],[-74.00311,40.70767],[-74.00336,40.708],[-74.00345,40.70808],[-74.00407,40.70745],[-74.00412,40.70757],[-74.00433,40.70783],[-74.00477,40.70841],[-74.00505,40.70876],[-74.00513,40.70885],[-74.00524,40.70893],[-74.00532,40.70899],[-74.00547,40.70909],[-74.00643,40.70956],[-74.00705,40.70987],[-74.00774,40.71022],[-74.00906,40.71089],[-74.01046,40.71153],[-74.01013,40.71209],[-74.00967,40.71274],[-74.00927,40.71326],[-74.00902,40.71359],[-74.00885,40.71381],[-74.0084,40.71437],[-74.00795,40.71494],[-74.00755,40.71544],[-74.00882,40.71602],[-74.0092,40.71619],[-74.00911,40.71692],[-74.00906,40.71726],[-74.009,40.7176],[-74.00894,40.71793],[-74.00888,40.71827],[-74.00882,40.71864],[-74.00875,40.71903],[-74.0087,40.7193],[-74.00858,40.71996],[-74.00847,40.72065],[-74.00842,40.72089],[-74.00837,40.7212],[-74.00834,40.72133],[-74.00823,40.72198],[-74.00812,40.72264],[-74.00801,40.72328],[-74.00795,40.72365],[-74.00793,40.72376],[-74.00786,40.72382],[-74.00777,40.72388],[-74.00773,40.72392],[-74.00771,40.72393],[-74.00745,40.72412],[-74.00736,40.72417],[-74.00728,40.72424],[-74.00723,40.72429],[-74.0071,40.72441],[-74.00703,40.7245]]}" +
+                "\"/>"  +
+                "<input type=\"submit\" value=\"deliveryDetail\"></form>" +
+
                 "<h4>OCI Streaming Service via Kafka API (orderstreaming service and order service)</h4>" +
                 "<form action=\"orderservicecall\">" +
                 "# of orders to stream : <input type=\"text\" name=\"numberoforderstostream\"  size=\5\" value=\"5\"> " +
