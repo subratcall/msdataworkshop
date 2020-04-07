0794b732fbfae0addaad43805fe2f89156cfdb65
Paul Parkinson <paul.parkinson@gmail.com>
2020-04-06 21:07:03 -0400

    saga services

---
+++ deploy.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+echo ________________________________________
+echo deploying frontend-helidon...
+echo ________________________________________
+cd frontend-helidon
+./deploy.sh
+cd ../
+
+echo ________________________________________
+echo deploying atpaqadmin...
+echo ________________________________________
+cd atpaqadmin
+./deploy.sh
+cd ../
+
+echo ________________________________________
+echo deploying order-helidon...
+echo ________________________________________
+cd order-helidon
+./deploy.sh
+cd ../
+
+echo ________________________________________
+echo deploying inventory-helidon...
+echo ________________________________________
+cd inventory-helidon
+./deploy.sh
+cd ../
+
+echo ________________________________________
+echo deploying supplier-helidon-se...
+echo ________________________________________
+cd supplier-helidon-se
+./deploy.sh
+cd ../
+
+echo ________________________________________
+echo deploying orderstreaming-helidon-se...
+echo ________________________________________
+cd orderstreaming-helidon-se
+./deploy.sh
+cd ../
+
+echo ________________________________________
+echo ...finished
+echo ________________________________________
