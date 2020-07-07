a6431a0d17cec036e3929140c937c3b2466582bd
Paul Parkinson <paul.parkinson@gmail.com>
2020-07-06 23:06:09 -0400

    setup without OSB

--- osb-atp-and-oss/setupATPWithoutOSBInventoryDB.sh
+++ osb-atp-and-oss/setupATPWithoutOSBInventoryDB.sh
@@ -12,14 +12,14 @@ cd inventorydbwallet
 oci db autonomous-database generate-wallet --autonomous-database-id ocid1.autonomousdatabase.oc1.phx.abyhqljsykgg4c5ou2yllx6pkt76nxppmt3wbmx2hwztkxkgmpjatz6fsxqq --file inventorydbwallet.zip --password Welcome_123
 unzip inventorydbwallet.zip
 echo "for inventory admin and inventoryuser secrets..."
-export cwallet_sso=$(cat cwallet.sso | base64)
-export ewallet_p12=$(cat ewallet.p12 | base64)
-export keystore_jks=$(cat keystore.jks | base64)
-export ojdbc_properties=$(cat ojdbc.properties | base64)
-export README=$(cat README | base64)
-export sqlnet_ora=$(cat sqlnet.ora | base64)
-export tnsnames_ora=$(cat tnsnames.ora | base64)
-export truststore_jks=$(cat truststore.jks | base64)
+export cwallet_sso=$(cat cwallet.sso | base64 -w 0)
+export ewallet_p12=$(cat ewallet.p12 | base64 -w 0)
+export keystore_jks=$(cat keystore.jks | base64 -w 0)
+export ojdbc_properties=$(cat ojdbc.properties | base64 -w 0)
+export README=$(cat README | base64 -w 0)
+export sqlnet_ora=$(cat sqlnet.ora | base64 -w 0)
+export tnsnames_ora=$(cat tnsnames.ora | base64 -w 0)
+export truststore_jks=$(cat truststore.jks | base64 -w 0)
 rm inventorydbwallet.zip
 cd ../
 
