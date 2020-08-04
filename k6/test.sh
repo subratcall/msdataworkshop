814b60c16b304f6b097fc6885e0d9d5e7328f1b8
Paul Parkinson <paul.parkinson@gmail.com>
2020-08-04 19:05:59 -0400

    opentracing

--- k6/test.sh
+++ k6/test.sh
@@ -1,15 +1,22 @@
-export RUN="$1"
-export VUS="$2"
-export DURATION="5"
+export VUS="$1"
+export DURATION="20"
 
 set -e
 
-case "$RUN" in
-    ''|*[!0-9]*) 
-        echo "Run number must be a positive integer"
-        exit
-    ;;
-esac
+if [[ ! $LB =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
+    echo "Set variable LB to the external IP address of the ext-order service"
+    exit
+fi
+
+NEXT_RUN_FILE=next_run
+
+if [ ! -f "$NEXT_RUN_FILE" ]; then
+    echo 0 > $NEXT_RUN_FILE
+fi
+
+export RUN=`cat $NEXT_RUN_FILE`
+RUN=$((RUN+1))
+echo $RUN > $NEXT_RUN_FILE
 
 mkdir "RUN${RUN}"
 
