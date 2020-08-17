1536ad79bf094d64120c2b1bc0ab3c46736d60ff
Paul Parkinson <paul.parkinson@gmail.com>
2020-08-17 18:31:18 -0400

    automation work for lab 1 and 2

--- addAndSourcePropertiesInBashrc.sh
+++ addAndSourcePropertiesInBashrc.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# we put a placeholder msdataworkshopjaegeraddress.txt as we wont have the Jaeger LB address until later/setJaegerAddress.sh step
+touch workingdir/msdataworkshopjaegeraddress.txt
 echo "export MSDATAWORKSHOP_LOCATION=~/msdataworkshop-master/" >> ~/.bashrc
 echo "source ~/msdataworkshop-master/msdataworkshop.properties" >> ~/.bashrc
 source ~/.bashrc
